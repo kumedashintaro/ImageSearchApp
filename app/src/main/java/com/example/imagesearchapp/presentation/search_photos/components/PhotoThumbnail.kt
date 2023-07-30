99c9b685d63fab424369f36f0d67ab1a498f9ae8
kumedashintaro <pii.k2.bys@gmail.com>
2023-07-30 23:20:14 +0900

    add Icon and Image

--- app/src/main/java/com/example/imagesearchapp/presentation/search_photos/components/PhotoThumbnail.kt
+++ app/src/main/java/com/example/imagesearchapp/presentation/search_photos/components/PhotoThumbnail.kt
@@ -5,16 +5,24 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.imagesearchapp.domain.model.Photo
+import com.example.imagesearchapp.presentation.ui.theme.ImageSearchAppTheme
 
 @Composable
 fun PhotoThumbnail(
@@ -27,6 +35,11 @@ fun PhotoThumbnail(
             .clickable { onClick(photo) },
         contentAlignment = Alignment.BottomCenter
     ) {
+        AsyncImage(
+            model = photo.imageUrl,
+            contentDescription = photo.description,
+            modifier = Modifier.fillMaxWidth()
+        )
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -48,7 +61,34 @@ fun PhotoThumbnail(
                     style = MaterialTheme.typography.bodyMedium
                 )
             }
-
+            Spacer(modifier = Modifier.weight(1f))
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = "likes",
+                tint = Color.Magenta
+            )
+            Spacer(modifier = Modifier.width(5.dp))
+            Text(
+                text = photo.likes.toString(),
+                color = Color.White,
+                style = MaterialTheme.typography.bodySmall
+            )
         }
+    }
+}
+
+@Preview
+@Composable
+private fun PhotoThumbnailPreview() {
+    val photo = Photo(
+        photoId = "",
+        description = " description",
+        likes = 100,
+        imageUrl = "https://unsplash.com/ja/%E5%86%99%E7%9C%9F/%E3%83%97%E3%83%BC%E3%83%AB%E3%81%AE%E7%AB%AF%E3%81%AB%E5%BA%A7%E3%81%A3%E3%81%A6%E3%81%84%E3%82%8B%E4%B8%80%E6%9D%AF%E3%81%AE%E3%82%B3%E3%83%BC%E3%83%92%E3%83%BC-QxETQsRocGg",
+        photographer = "photographer"
+    )
+    
+    ImageSearchAppTheme {
+        PhotoThumbnail(photo = photo, onClick = {})
     }
 }
