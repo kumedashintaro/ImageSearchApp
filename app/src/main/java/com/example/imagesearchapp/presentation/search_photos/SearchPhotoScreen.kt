254e8cb2c2cfce63bd89cfc6177dee686cc6387a
kumedashintaro <pii.k2.bys@gmail.com>
2023-08-06 17:39:31 +0900

    add SearchB

--- app/src/main/java/com/example/imagesearchapp/presentation/search_photos/SearchPhotoScreen.kt
+++ app/src/main/java/com/example/imagesearchapp/presentation/search_photos/SearchPhotoScreen.kt
@@ -1,21 +1,34 @@
 package com.example.imagesearchapp.presentation.search_photos
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.imagesearchapp.presentation.search_photos.components.PhotoThumbnail
-import dagger.Lazy
+import com.example.imagesearchapp.presentation.search_photos.components.SearchBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchPhotoScreen(
     viewModel: SearchPhotoViewModel = hiltViewModel(),
-){
+) {
     val state = viewModel.state.value
-    LazyColumn{
-        items(state.photos){ photo ->
-            PhotoThumbnail(photo = photo, onClick = {})
+
+    Scaffold(
+        topBar = {
+            SearchBar(searchText = "",
+                onSearchTextChanged = {},
+                onDone = { /*TODO*/ })
+        }
+    ) { paddingValue ->
+        LazyColumn(modifier = Modifier.padding(paddingValue)) {
+            items(state.photos) { photo ->
+                PhotoThumbnail(photo = photo, onClick = {})
+            }
         }
     }
 }
